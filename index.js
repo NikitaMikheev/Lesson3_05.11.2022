82c0351a0251a34d02d344f344b3fee0c54f26e9
NikitaMikheev <nikita.miheev.1999@mail.ru>
2022-11-05 09:55:23 +0300

    Занятие 3

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-let age = 17
-
-if (age>=18) {
-    console.log("Вход разрешен")   
-} else {
-    console.log("Вход запрещен")   
-}
-
-
+let age = 18
 let gender = "Мужской";
 
+    if (age>18) 
+    {
+        console.log("Добро пожаловать")
+    } 
+
+    else if (age==18)
+    {
+        console.log("Добро пожаловать. Поздравляем вас с совершеннолетием!")   
+    }
+
+    else
+    {
+        console.log("Вход запрещен")   
+    }
+
+
 if (gender.toLocaleLowerCase() === "мужской") {  /* Приводит к нижнему регистру переменную gender*/
-    console.log("Вам направо");
+console.log("Вам направо");
 }
- 
+
 if (gender === "женский") {
     console.log("Вам налево");
-}
+}   
