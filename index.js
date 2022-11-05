736bee69c1d190e6fa02e959315d88e0881150ec
NikitaMikheev <nikita.miheev.1999@mail.ru>
2022-11-05 09:49:10 +0300

    Занятие 3

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+let age = 17
+
+if (age>=18) {
+    console.log("Вход разрешен")   
+} else {
+    console.log("Вход запрещен")   
+}
+
 
 let gender = "Мужской";
 
-if (gender.toLocaleLowerCase() === "мужской") {
+if (gender.toLocaleLowerCase() === "мужской") {  /* Приводит к нижнему регистру переменную gender*/
     console.log("Вам направо");
 }
  
